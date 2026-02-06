13770a9efdecfc203aa168a1a7f7f35ff9016ffb
Okopr1va <pro.dreamyt@gmail.com>
2026-02-06 13:28:03 +0100

    visual studio

---
+++ utulek/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using utulek.UI;
+
+namespace utulek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KonzoleUI ui = new KonzoleUI();
+            ui.Spust();
+        }
+    }
+}
